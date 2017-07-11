72ef4a48ef4670a6c8a9bf835cbe8ff1b493fcfc
atcrew <atcrew3@gmail.com>
2017-07-11 14:23:35 +0300

    po
    po

--- routes/PostOffice.js
+++ routes/PostOffice.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient,
-	assert = require('assert');
+	    assert = require('assert');
 var mongodb = require('mongodb');
 var crypto = require('crypto');
 var settings=require('./settings');
@@ -10,70 +10,79 @@ var settings=require('./settings');
 //var url = 'mongodb://localhost:27017/test';
 var url=settings.dbURL;
 /* GET all users*/
-router.get('/all', function(req, res, next) {
+router.get('/all', function (req, res, next) {
 
-	MongoClient.connect(url, function(err, db) {
-		var collection = db.collection('postoffices');
-			collection.aggregate([							
-				{
-				  $lookup:{	from:"evnt",
-							localField:"postalCode",
-							foreignField:"postalCode", 
-							as:"evnts"}
-				}
-				]).toArray().then(function(r){
+	    MongoClient.connect(url, function (err, db) {
+		    var collection = db.collection('postoffices');
+			    collection.aggregate([							
+				    {
+				          $lookup:{from:'evnt',
+							localField:'postalCode',
+							foreignField:'postalCode', 
+							as:'evnts'}
+				    }
+				]).toArray().then(function (r) {
 
-				db.close();							
-				res.setHeader('Last-Modified', (new Date()).toUTCString());
-				return res.json(r);
+				    db.close();							
+				    res.setHeader('Last-Modified', (new Date()).toUTCString());
+				    return res.json(r);
 
 			});
 	});
 });
 
-router.get('/search', function(req, res, next) {
+router.get('/search', function (req, res, next) {
 
-	MongoClient.connect(url, function(err, db) {
-		var collection = db.collection('postoffices');
-			collection.aggregate([							
-				{
-				  $lookup:{	from:"evnt",
-							localField:"postalCode",
-							foreignField:"postalCode", 
-							as:"evnts"}
-				},				
-				{
-					$match:{
-						"evnts.title":"123"
+	    MongoClient.connect(url, function (err, db) {
+		    var collection = db.collection('postoffices');
+			    collection.aggregate([							
+				    {
+				          $lookup:{from:'evnt',
+							localField:'postalCode',
+							foreignField:'postalCode', 
+							as:'evnts'}
+				    },				
+				    {
+					        $match:{
+						'evnts.title':"123"
 					}
-				}
-				]).toArray().then(function(r){
+				    }
+				]).toArray().then(function (r) {
 
-				db.close();							
-				res.setHeader('Last-Modified', (new Date()).toUTCString());
-				return res.json(r);
+				    db.close();							
+				    res.setHeader('Last-Modified', (new Date()).toUTCString());
+				    return res.json(r);
 
 			});
 	});
 });
-
-router.post('/modify', function (req, res, next) {
-    
+router.post('/new',function (req,res) {
+	MongoClient.connect(url, function (err, db) {
+    	assert.equal(null, err);
+    	db.collection('postoffices').insertOne(req.body).then(function (po) {
+					            assert.equal(1, po.insertedCount);
+					            res.json({
+					            	'insert': 'ok',
+					            	'id': po.insertedId
+					            });
+		});
+	});
+});
+router.post('/update', function (req, res, next) {    
     MongoClient.connect(url, function (err, db) {
         assert.equal(null, err);
-        console.log('Connected successfully to server');
         db.collection('postoffices').updateOne({
-            _id: new mongodb.ObjectID(req.params.id)
-					// title:'111111'
+            _id: new mongodb.ObjectID(req.body.id)
         }, {
             $set: {
-                title: req.body.title,
-                start: req.body.start,
-                end: req.body.end,
+                label: req.body.postalCode,
                 postalCode: req.body.postalCode,
-                status: req.body.status,
-                description: req.body.description,
-                executor: req.body.executor
+                latitude: req.body.latitude,
+                longitude: req.body.longitude,
+                addressSource: req.body.addressSource,
+                region: req.body.region,
+                settlement: req.body.settlement,
+                comps: req.body.comps
             }
         },
 			function (err, r) {
@@ -91,5 +100,20 @@ router.post('/modify', function (req, res, next) {
 
 });
 
+router.post('/del', function(req, res, next) {
+	MongoClient.connect(url, function(err, db) {		
+		var collection = db.collection('postoffices');
+		collection.findOneAndDelete({_id: new mongodb.ObjectID(req.body._id)}).then(function(r) {
+			test.equal(1, r.lastErrorObject.n);
+        	test.equal(req.body._id, r.value._id);
+			return res.json({
+						"status":"ok",
+						"text": "Почтовое отделение удалено"
+					});
+
+			db.close();	
+		});
+	});
+});
 
 module.exports = router;
